807c6be9ec2af55bd5354d2337c876944fe54466
Alrainn <melihawci@gmail.com>
2025-05-21 16:21:56 +0300

    lombok-update-2
    user sınıfına getter setterlar manuel olarak eklendi

--- src/main/java/com/melihawci/springrestapi/model/User.java
+++ src/main/java/com/melihawci/springrestapi/model/User.java
@@ -12,4 +12,37 @@ public class User {
     private String username;
     private String email;
     private String password;
+
+    // Manuel getter ve setter metodları
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 } 
